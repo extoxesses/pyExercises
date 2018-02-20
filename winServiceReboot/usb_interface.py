1990dc4869bada05c2751536f32888247eafa6d5
exto <extoxesses@gmail.com>
2018-02-20 22:27:20 +0100

    Added usb python script

---
+++ winServiceReboot/usb_interface.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#
+# File: win_service_reboot.py
+# Author: eXtoxesses
+# Date: February 18, 2018
+#
+# (C) COPYRIGHT eXtoxesses 2018
+# Released under the same license as Python 3.6.4
+
+import usb
+
+# https://stackoverflow.com/questions/12542799/communication-with-the-usb-device-in-python
+usb.core.find()
